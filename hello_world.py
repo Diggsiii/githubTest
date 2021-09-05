03849a7f1efbdf31304b5643cebc6482f5e1719f
Randolph S. Diggs III <rd3@me.com>
2021-09-04 20:16:54 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -8,4 +8,7 @@ print("hello, world 2")
 print("hi")
 print("lol")
 print("test1")
+<<<<<<< HEAD
+>>>>>>> parent of d596939 (Update hello_world.py)
+=======
 >>>>>>> parent of d596939 (Update hello_world.py)
