5baf8c19c6953b2fee06451c183e159aec3fa000
Randolph S. Diggs III <rd3@me.com>
2021-09-04 20:17:32 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+#<<<<<<< HEAD
 name = "Q"
 =======
 
@@ -8,7 +8,7 @@ print("hello, world 2")
 print("hi")
 print("lol")
 print("test1")
-<<<<<<< HEAD
->>>>>>> parent of d596939 (Update hello_world.py)
-=======
->>>>>>> parent of d596939 (Update hello_world.py)
+#<<<<<<< HEAD
+#>>>>>>> parent of d596939 (Update hello_world.py)
+#=======
+#>>>>>>> parent of d596939 (Update hello_world.py)
